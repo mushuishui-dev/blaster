0569d9dc5ad153b1c85ea0f14ad75ec47c5ebc18
mushuishui-dev <731000193@qq.com>
2024-12-03 09:30:45 +0800

    Add crosshairs

--- Source/Blaster/HUD/BlasterHUD.cpp
+++ Source/Blaster/HUD/BlasterHUD.cpp
@@ -6,4 +6,40 @@
 void ABlasterHUD::DrawHUD()
 {
 	Super::DrawHUD();
+	FVector2d ViewportSize;
+	if (GEngine)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+		const FVector2d ViewportCenter((ViewportSize.X / 2.f), ViewportSize.Y / 2.f);
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsCenter, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsLeft)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsLeft, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsRight)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsRight, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsTop)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsTop, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsBtoom)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsBtoom, ViewportCenter);
+		}
+	}
+}
+
+void ABlasterHUD::DrawCrosshair(UTexture2D* Texture, FVector2d ViewportCenter)
+{
+	// 获取纹理尺寸
+	const float TextureWidth = Texture->GetSizeX();
+	const float TextureHeight = Texture->GetSizeY();
+	const FVector2d TextureDrawPoint(ViewportCenter.X - TextureWidth / 2.f, ViewportCenter.Y - TextureHeight / 2.f);
+	// 绘制贴图
+	DrawTexture(Texture, TextureDrawPoint.X, TextureDrawPoint.Y, TextureWidth, TextureHeight, 0.f, 0.f, 1.f, 1.f);
 }
