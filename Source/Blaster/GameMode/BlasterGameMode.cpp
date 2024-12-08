5b83cacd8573a2aa9e9bc9b977cd81ccff313d71
mushuishui-dev <731000193@qq.com>
2024-12-08 11:05:50 +0800

    Add warmup time

--- Source/Blaster/GameMode/BlasterGameMode.cpp
+++ Source/Blaster/GameMode/BlasterGameMode.cpp
@@ -7,6 +7,35 @@
 #include "GameFramework/PlayerStart.h"
 #include "Kismet/GameplayStatics.h"
 
+ABlasterGameMode::ABlasterGameMode()
+{
+	bDelayedStart = true;
+	
+}
+
+void ABlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LevelStartingTime = GetWorld()->GetTimeSeconds();
+	
+}
+
+void ABlasterGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (MatchState == MatchState::WaitingToStart)
+	{
+		CoutdownTime = WarmupTime - (GetWorld()->GetTimeSeconds() - LevelStartingTime);
+		if (CoutdownTime <= 0.f)
+		{
+			StartMatch();
+		}
+	}
+	
+}
+
 void ABlasterGameMode::PlayerEliminated(ABlasterCharacter* ElimmedCharacter, ABlasterPlayerController* VictimController,
                                         ABlasterPlayerController* AttackerController)
 {
--- Source/Blaster/GameMode/BlasterGameMode.h
+++ Source/Blaster/GameMode/BlasterGameMode.h
@@ -16,7 +16,21 @@ class BLASTER_API ABlasterGameMode : public AGameMode
 	GENERATED_BODY()
 
 public:
+	ABlasterGameMode();
+	virtual void Tick(float DeltaSeconds) override;
+	
 	virtual void PlayerEliminated(ABlasterCharacter* ElimmedCharacter, ABlasterPlayerController* VictimController, ABlasterPlayerController* AttackerController);
 
 	virtual void RequestRespawn(ACharacter* ElimeedCharacter, AController* ElimeedController);
+
+	UPROPERTY(EditDefaultsOnly)
+	float WarmupTime = 10.f;
+	float LevelStartingTime = 0.f;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	float CoutdownTime = 0.f;
+	
 };
